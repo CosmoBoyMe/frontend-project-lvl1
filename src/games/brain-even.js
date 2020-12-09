24c3c4f646e993a097b3f7c4ced02ff233a35a2a
CosmoBoyMe <2014failz2014@gmail.com>
2020-12-09 23:30:38 +0100

    move games to src

---
+++ src/games/brain-even.js
@@ -0,0 +1,15 @@
+import { getRandomNumber } from '../index.js';
+
+const isEven = (n) => (n % 2 === 0 ? 'yes' : 'no'); // is a number even?
+const getGameSetting = () => {
+  const text = 'Answer "yes" if the number is even, otherwise answer "no".';
+  const expressions = [];
+  const correctAnswers = [];
+  for (let i = 0; i < 3; i += 1) {
+    const randomNumber = getRandomNumber(1, 100);
+    expressions.push(randomNumber);
+    correctAnswers.push(isEven(expressions[i]));
+  }
+  return { text, expressions, correctAnswers };
+};
+export default getGameSetting;
---
+++ src/games/brain-prime.js
@@ -0,0 +1,23 @@
+import { getRandomNumber } from '../index.js';
+
+const isPrime = (number) => {
+  for (let denom = 2; denom < number; denom += 1) {
+    if (number % denom === 0) {
+      return 'no';
+    }
+  }
+  return number < 2 ? 'no' : 'yes';
+};
+const getGameSetting = () => {
+  const text = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+  const primeNumbers = [];
+  const correctAnswers = [];
+  for (let i = 0; i < 3; i += 1) {
+    const randomNumber = getRandomNumber(1, 100);
+    primeNumbers.push(randomNumber);
+    correctAnswers.push(isPrime(randomNumber));
+  }
+  return { text, primeNumbers, correctAnswers };
+};
+
+export default getGameSetting;
